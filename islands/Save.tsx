77f239780e49daba686019e584efff98e7cf7492
YuseiSato0302 <2123065@ed.tus.ac.jp>
2024-03-10 17:06:25 +0900

    improve save.tsx

--- islands/Save.tsx
+++ islands/Save.tsx
@@ -8,7 +8,7 @@ interface CounterProps {
 export default function Counter(props: CounterProps) {
   return (
     <div class="flex gap-8 py-6">
-      <Button onClick={() =>alert("保存しました")}>保存する</Button>
+      <Button onClick={() => alert("保存しました")}>保存する</Button>
     </div>
   );
 }
