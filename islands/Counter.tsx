dfd37bfb3baf2409a8607fe65d3db8276235e056
Hironori Wada <hironori104@gmail.com>
2024-03-10 16:18:37 +0900

    format

--- islands/Counter.tsx
+++ islands/Counter.tsx
@@ -8,9 +8,13 @@ interface CounterProps {
 export default function Counter(props: CounterProps) {
   return (
     <div class="flex gap-8 py-6">
-      <Button onClick={() => props.count.value = props.count.value / 2}>÷2</Button>
+      <Button onClick={() => props.count.value = props.count.value / 2}>
+        ÷2
+      </Button>
       <p class="text-3xl tabular-nums">{props.count}</p>
-      <Button onClick={() => props.count.value = props.count.value * 2}>×2</Button>
+      <Button onClick={() => props.count.value = props.count.value * 2}>
+        ×2
+      </Button>
     </div>
   );
 }
