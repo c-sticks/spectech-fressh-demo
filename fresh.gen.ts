470df6b1153ab0c0daed0f5d57fb4a6422a0ea36
YuseiSato0302 <2123065@ed.tus.ac.jp>
2024-03-10 16:15:01 +0900

    create saveBotton

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -7,7 +7,9 @@ import * as $_app from "./routes/_app.tsx";
 import * as $api_joke from "./routes/api/joke.ts";
 import * as $greet_name_ from "./routes/greet/[name].tsx";
 import * as $index from "./routes/index.tsx";
+import * as $save from "./routes/save.tsx";
 import * as $Counter from "./islands/Counter.tsx";
+import * as $Save from "./islands/Save.tsx";
 import { type Manifest } from "$fresh/server.ts";
 
 const manifest = {
@@ -17,9 +19,11 @@ const manifest = {
     "./routes/api/joke.ts": $api_joke,
     "./routes/greet/[name].tsx": $greet_name_,
     "./routes/index.tsx": $index,
+    "./routes/save.tsx": $save,
   },
   islands: {
     "./islands/Counter.tsx": $Counter,
+    "./islands/Save.tsx": $Save,
   },
   baseUrl: import.meta.url,
 } satisfies Manifest;
