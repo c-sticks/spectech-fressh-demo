1eff3673ec86d698e8b80faab8e3eee8b06ab575
Hideki <kefkef3939@gmail.com>
2024-03-10 16:20:41 +0900

    feature_b

---
+++ routes/test1.tsx
@@ -0,0 +1,21 @@
+import type { Signal } from "@preact/signals";
+import { Button } from "../components/Button.tsx";
+
+export function Test(){
+    const handleClick = () => {
+        // GoogleのURLにリダイレクト
+        window.location.href = "https://google.co.jp/";
+    };
+    return(
+        <div class="px-4 py-8 mx-auto bg-[#86efac]">
+            <h1>welcome to test page</h1>
+            <div class="flex gap-8 py-6">
+                <Button onClick={handleClick}>waa</Button>
+
+            </div>
+
+
+
+        </div>
+    );
+}
